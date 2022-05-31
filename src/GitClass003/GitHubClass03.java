db840bf706dc2b90571eb7ae7a161db5c4a6b697
Bilal NYC <bilaldost7@gmail.com>
2022-05-31 19:54:29 -0400

    2nd commit

--- src/GitClass003/GitHubClass03.java
+++ src/GitClass003/GitHubClass03.java
@@ -3,8 +3,15 @@ package GitClass003;
 public class GitHubClass03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+// Tracking out Project in git.
+// navigate our terminal to the location where we want to create the repository
+		System.out.println("Find my GitHub Coding");
 
+		// git add . -->tells git which files to track
+		
+		System.out.println("Today is the best day");
+		// when i update the code this will be my GitHub Command -->
 	}
 
 }
